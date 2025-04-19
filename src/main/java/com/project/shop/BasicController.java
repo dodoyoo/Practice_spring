e55c4e67a129834daf390efa45d354d5133b566c
dodoyoo <dong15903@daum.net>
2025-04-19 17:52:59 +0900

    Connect GitHub

---
+++ src/main/java/com/project/shop/BasicController.java
@@ -0,0 +1,15 @@
+package com.project.shop;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class BasicController {
+    @GetMapping("/")
+    @ResponseBody
+    String hello() {
+        return "oellh";
+    }
+
+}
